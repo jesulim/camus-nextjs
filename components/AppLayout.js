063ec62ade769c42005e5eb0911c8533cd366e63
Ivan Martinez <jesulim@gmail.com>
2022-12-06 19:20:36 -0400

    change: 85%vh

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -2,7 +2,7 @@ export default function AppLayout ({ children }) {
   return (
     <>
       <div className='bg-gray-500 grid place-content-center h-screen'>
-        <main className='bg-gray-300 rounded-xl shadow-2xl w-[370px] h-[80vh] overflow-y-auto'>
+        <main className='bg-gray-300 rounded-xl shadow-2xl w-[370px] h-[85vh] overflow-y-auto'>
           {children}
         </main>
       </div>
